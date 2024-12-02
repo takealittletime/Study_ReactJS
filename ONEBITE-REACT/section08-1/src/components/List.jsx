b867a8d844de5e6817371d3d109f9c3b0589da9d
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-02 18:54:32 +0900

    useReducer 학습

---
+++ ONEBITE-REACT/section08-1/src/components/List.jsx
@@ -0,0 +1,41 @@
+import './List.css'
+import TodoItme from './Todoitem';
+import { useState } from 'react';
+
+const List = ({todos, onUpdate, onDelete}) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e)=>{
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = ()=>{
+    // 검색어가 없을 때 -> todos 모두 반환
+    if (search == ""){
+      return todos;
+    }
+    // 검색어가 있을 때-> 검색어로 filtering된 결과 반환
+    return todos.filter((todo)=>
+      todo.content.toLowerCase().includes(search.toLowerCase()));
+  }
+
+  const filteredTodos = getFilteredData();
+
+  return (
+  <div className="List">
+    <h4>Todo List 🌱</h4>
+    <input
+      value={search}
+      onChange={onChangeSearch} 
+      placeholder="검색어를 입력하세요"
+    />
+    <div className='todo_wrapper'>
+      {filteredTodos.map((todo)=>{
+        return <TodoItme key={todo.id} {...todo} onUpdate = {onUpdate} onDelete = {onDelete}/>;
+      })}
+    </div>
+  </div>
+  );
+}
+
+export default List;
---
+++ ONEBITE-REACT/section08-1/src/components/Todoitem.jsx
@@ -0,0 +1,33 @@
+import './Todoitem.css'
+
+const TodoItme = ({
+  id, 
+  isDone, 
+  content, 
+  date, 
+  onUpdate, 
+  onDelete})=> {
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  }
+
+  const onClickDeleteButton = ()=>{
+    onDelete(id);
+  }
+
+  return (
+    <div className='TodoItem'>
+      <input
+        onChange={onChangeCheckbox} 
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date (date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  )
+}
+
+export default TodoItme
