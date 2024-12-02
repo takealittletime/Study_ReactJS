f7a13d44bb12a1cc145931083854b6bbc3f1d028
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-02 10:45:49 +0900

    useState를 이용한 사용자 입력 받기

---
+++ ONEBITE-REACT/section05-1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Register from "./components/Register"
+
+function App() {
+  // console.log(state);
+
+  return (
+    <>
+      <Register/>
+    </>
+  )
+}
+
+export default App
