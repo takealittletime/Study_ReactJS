156c9fb7ed9443b37ef9770ae53ac9b75c759e0a
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-02 12:49:43 +0900

    ReactHook 이용하기

--- ONEBITE-REACT/section05-1/src/App.jsx
+++ ONEBITE-REACT/section05-1/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css"
+import HookExam from "./components/HookExam"
 import Register from "./components/Register"
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <Register/>
+      <HookExam/>
     </>
   )
 }
--- ONEBITE-REACT/section05-1/src/components/HookExam.jsx
+++ ONEBITE-REACT/section05-1/src/components/HookExam.jsx
@@ -1,5 +1,21 @@
+import useInput from "./../hooks/useinput";
+// 3가지 hook 관련 팁
+// 1. 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능
+// 2. 조건부로 호출될 수는 없다.
+// 3. 나만의 훅(Custom Hook)을 직접 만들 수 있다.
+
+// 함수 이름 앞에 접두사 use를 붙이면 자동으로 커스텀 훅으로 인식.
+
+
 const HookExam = () => {
-  return <div>Hookexam</div>
+
+  const [input, onChange] = useInput();
+
+  return (
+    <div>
+      <input value = {input} onChange={onChange} />
+    </div>
+  );
 }
 
 export default HookExam;
