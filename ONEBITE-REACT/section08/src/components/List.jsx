c887545a5e42136e86725cb377087934068f490a
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-02 16:16:11 +0900

    ToDoList만들기_1.0.1_검색조건필터링

--- ONEBITE-REACT/section08/src/components/List.jsx
+++ ONEBITE-REACT/section08/src/components/List.jsx
@@ -1,15 +1,38 @@
 import './List.css'
 import TodoItme from './Todoitem';
+import { useState } from 'react';
 
-const List = () => {
+const List = ({todos}) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e)=>{
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = ()=>{
+    // 검색어가 없을 때 -> todos 모두 반환
+    if (search == ""){
+      return todos;
+    }
+    // 검색어가 있을 때-> 검색어로 filtering된 결과 반환
+    return todos.filter((todo)=>
+      todo.content.toLowerCase().includes(search.toLowerCase()));
+  }
+
+  const filteredTodos = getFilteredData();
+
   return (
   <div className="List">
     <h4>Todo List 🌱</h4>
-    <input placeholder="검색어를 입력하세요" />
+    <input
+      value={search}
+      onChange={onChangeSearch} 
+      placeholder="검색어를 입력하세요"
+    />
     <div className='todo_wrapper'>
-      <TodoItme/>
-      <TodoItme/>
-      <TodoItme/>
+      {filteredTodos.map((todo)=>{
+        return <TodoItme key={todo.id} {...todo}/>;
+      })}
     </div>
   </div>
   );
