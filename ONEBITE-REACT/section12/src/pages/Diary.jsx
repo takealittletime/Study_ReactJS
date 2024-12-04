00bd240868c1c9a642611afe7e719bb7444ffe2d
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-05 01:42:03 +0900

    URL Parameter를 이용해 동적 경로 설정하기

--- ONEBITE-REACT/section12/src/pages/Diary.jsx
+++ ONEBITE-REACT/section12/src/pages/Diary.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const Diary= ()=> {
+  const params = useParams();
+  console.log(params);
+
   return (
     <div>
-      <h1>Diary</h1>
+      <h1>{params.id}번 일기입니다!</h1>
     </div>
   )
 };
