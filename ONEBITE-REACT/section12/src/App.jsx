c850df7329428ea188ebac41ac6ffa1e13ed1ff9
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-05 02:21:33 +0900

    CSS설정하기

--- ONEBITE-REACT/section12/src/App.jsx
+++ ONEBITE-REACT/section12/src/App.jsx
@@ -5,11 +5,7 @@ import Diary from './pages/Diary';
 import New from './pages/New';
 import NotFound from './pages/Notfound';
 
-import emotion1 from "./assets/emotion1.png"
-import emotion2 from "./assets/emotion2.png"
-import emotion3 from "./assets/emotion3.png"
-import emotion4 from "./assets/emotion4.png"
-import emotion5 from "./assets/emotion5.png"
+import {getEmotionImage} from "./util/get-emotion-image";
 
 
 // 1. "/": 모든 일기를 조회하는 home 페이지
@@ -24,21 +20,11 @@ function App() {
   return (
     <>
       <div>
-        <h1>아래는 public 폴더에서 가져온 이미지</h1>
-        <img src={'/emotion1.png'}/>
-        <img src={'/emotion2.png'}/>
-        <img src={'/emotion3.png'}/>
-        <img src={'/emotion4.png'}/>
-        <img src={'/emotion5.png'}/>
-      </div>
-      <div>
-        <h1>아래는 assets 폴더에서 가져온 이미지</h1>
-        {/* assets 폴더에 넣어서 data uri를 통해 최적화하는 기능(vite가 제공)을 사용하는 것이 좋다 */}
-        <img src={emotion1}/>
-        <img src={emotion2}/>
-        <img src={emotion3}/>
-        <img src={emotion4}/>
-        <img src={emotion5}/>
+        <img src={getEmotionImage(1)}/>
+        <img src={getEmotionImage(2)}/>
+        <img src={getEmotionImage(3)}/>
+        <img src={getEmotionImage(4)}/>
+        <img src={getEmotionImage(5)}/>
       </div>
       <div>
         <Link to = {"/"}>Home</Link>
