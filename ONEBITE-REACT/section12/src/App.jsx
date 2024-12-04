e74782aa0fec6e997e20eead0f0f1e8f58855ddc
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-05 01:36:43 +0900

    useNavigate를 이용해 페이지 이동하기

--- ONEBITE-REACT/section12/src/App.jsx
+++ ONEBITE-REACT/section12/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route, Link, useNavigate} from "react-router-dom";
 import Home from "./pages/Home"
 import Diary from './pages/Diary';
 import New from './pages/New';
@@ -9,8 +9,12 @@ import NotFound from './pages/Notfound';
 // 1. "/": 모든 일기를 조회하는 home 페이지
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
-
 function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  }
+
   return (
     <>
       <div>
@@ -18,6 +22,7 @@ function App() {
         <Link to = {"/new"}>New</Link>
         <Link to = {"/diary"}>Diary</Link>
       </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/new' element={<New/>}/>
