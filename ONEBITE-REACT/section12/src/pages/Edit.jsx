0a5181fd5ac4fa1bd883f5687e7255f6f1d70fe0
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-08 18:42:06 +0900

    New페이지 구현, Edit 페이지 구현_0.0

--- ONEBITE-REACT/section12/src/pages/Edit.jsx
+++ ONEBITE-REACT/section12/src/pages/Edit.jsx
@@ -1,11 +1,40 @@
-import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Edit = () => {
   const params = useParams();
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (
+      window.confirm('일기를 정말 삭제하시겠습니까? 다시 복구되지 않습니다!')
+    ){
+      // 일기 삭제 로직
+      onDelete(params.id);
+      nav('/',{replace:true});
+    }
+  };
 
   return (
     <div>
-      <h1>수정 페이지:: {params.id}번 일기입니다.</h1>
+      <Header
+        title={'일기 수정하기'}
+        leftChild={<Button onClick={()=> nav(-1)} text={'< 뒤로 가기'}/>}
+        rightChild={
+        <Button
+          onClick={onClickDelete}
+          text={'< 삭제하기'} 
+          type={'NEGATIVE'}
+          />
+        }
+      />
+      <Editor/>
     </div>
   )
 }
